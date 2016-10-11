db64f791dbec439967970d1d66c32b54bbafe70f
Sergey Sirosh <ssv.ruby@gmail.com>
2016-10-11 12:35:33 +0300

    New resource :train_routes

---
+++ app/models/train_route.rb
@@ -0,0 +1,3 @@
+class TrainRoute < ApplicationRecord
+  validates :title, presence:  true
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161008135647) do
+ActiveRecord::Schema.define(version: 20161011075715) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
 
   create_table "railway_stations", force: :cascade do |t|
+    t.string   "title"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "train_routes", force: :cascade do |t|
     t.string   "title"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
