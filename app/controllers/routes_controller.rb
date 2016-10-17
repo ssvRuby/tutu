a4e6610eaad6f510ccd66dae8225aebbde17f316
Sergey Sirosh <ssv.ruby@gmail.com>
2016-10-17 17:55:48 +0300

    Lesson 13

--- app/controllers/routes_controller.rb
+++ app/controllers/routes_controller.rb
@@ -44,7 +44,7 @@ class RoutesController < ApplicationController
   end
 
   def train_route_params
-    params.require(:route).permit(:title)
+    params.require(:route).permit(:title, railway_station_ids: [])
   end
 
 end
