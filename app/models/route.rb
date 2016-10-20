3c9b4cd77563fe6e69e7ba50de92fd2cafe7d40d
Sergey Sirosh <ssv.ruby@gmail.com>
2016-10-20 21:03:31 +0300

    Fel

--- app/models/route.rb
+++ app/models/route.rb
@@ -3,5 +3,7 @@ class Route < ApplicationRecord
   has_many :railway_stations_routes
   has_many :railway_stations, through: :railway_stations_routes
 
-  validates :title, presence:  true
+  validates :title, presence: true
+
+
 end
