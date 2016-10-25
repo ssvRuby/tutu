75b365353469a2fd70f8839ba2b5382a23de3c47
Sergey Sirosh <ssv.ruby@gmail.com>
2016-10-25 15:55:48 +0300

    Lesson 15 Finish 03

--- app/models/carriage.rb
+++ app/models/carriage.rb
@@ -15,24 +15,30 @@ class Carriage < ApplicationRecord
   protected
 
   def set_carriage_number
-    carriage_number = 0
-
-    # carriage_list = Carriage.select('number').where(train_id: self.train.id).order(number: :asc)
-    carriage_list = train.carriages.sort { |x, y| x.number <=> y.number}
-    carriage_number = 1 if carriage_list.length == 0
-
-    if carriage_number == 0
-      for i in 0 .. carriage_list.length-1 do
-        number_present = carriage_list.detect { |c| c.number == i + 1 }
-        if number_present.nil?
-          carriage_number = i + 1
-          break
-        end
-      end
-    end
-
-    carriage_number = carriage_list.last.number + 1 if carriage_number == 0
-    self.number = carriage_number
+    #--------------------------------------------------------------------------
+    # Алгоритм присваивания номеров с заполнением разрывов в последовательности
+    #--------------------------------------------------------------------------
+    # carriage_number = 0
+    #
+    # carriage_list = train.carriages.order(number: :asc)
+    # carriage_number = 1 if carriage_list.length == 0
+    #
+    # if carriage_number == 0
+    #   for i in 0 .. carriage_list.length-1 do
+    #     number_present = carriage_list.detect { |c| c.number == i + 1 }
+    #     if number_present.nil?
+    #       carriage_number = i + 1
+    #       break
+    #     end
+    #   end
+    # end
+    #
+    # carriage_number = carriage_list.last.number + 1 if carriage_number == 0
+    # self.number = carriage_number
+    
+    carriage_max_number = train.carriages.maximum(:number)
+    carriage_max_number = 0 if carriage_max_number.nil?
+    self.number = carriage_max_number + 1
   end
 
 end
