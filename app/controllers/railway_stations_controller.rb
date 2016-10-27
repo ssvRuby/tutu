4a8085e27f8a00858aa1ef5487c0a782f7e37572
Sergey Sirosh <ssv.ruby@gmail.com>
2016-10-27 13:01:05 +0300

    del comments

--- app/controllers/railway_stations_controller.rb
+++ app/controllers/railway_stations_controller.rb
@@ -1,25 +1,20 @@
 class RailwayStationsController < ApplicationController
   before_action :set_railway_station, only: [:show, :edit, :update, :destroy]
 
-  # GET /railway_stations
   def index
     @railway_stations = RailwayStation.all
   end
 
-  # GET /railway_stations/1
   def show
   end
 
-  # GET /railway_stations/new
   def new
     @railway_station = RailwayStation.new
   end
 
-  # GET /railway_stations/1/edit
   def edit
   end
 
-  # POST /railway_stations
   def create
     @railway_station = RailwayStation.new(railway_station_params)
 
@@ -32,7 +27,6 @@ class RailwayStationsController < ApplicationController
     end
   end
 
-  # PATCH/PUT /railway_stations/1
   def update
     respond_to do |format|
       if @railway_station.update(railway_station_params)
@@ -43,7 +37,6 @@ class RailwayStationsController < ApplicationController
     end
   end
 
-  # DELETE /railway_stations/1
   def destroy
     @railway_station.destroy
     respond_to do |format|
@@ -52,7 +45,7 @@ class RailwayStationsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
+
     def set_railway_station
       @railway_station = RailwayStation.find(params[:id])
     end
