ca94a67c271ebd956a2e122ac797c86b622671c3
Sergey Sirosh <ssv.ruby@gmail.com>
2016-10-31 17:54:08 +0300

    Lesson_17 Complicated task

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,8 @@
            :recoverable, :rememberable, :confirmable, :validatable
 
     has_many :tickets
+
+    validates :first_name, presence: true
+    validates :last_name, presence: true
+
   end
