2316fe6af0058c3a158ec36ae3704b13a02fdcea
Sergey Sirosh <ssv.ruby@gmail.com>
2016-11-03 15:25:37 +0300

    deploy 04

--- db/migrate/20161012085421_rename_train_route_to_route.rb
+++ db/migrate/20161012085421_rename_train_route_to_route.rb
@@ -1,5 +1,5 @@
 class RenameTrainRouteToRoute < ActiveRecord::Migration[5.0]
-  def change
-    rename_table :train_routes, :routes
-  end
+  # def change
+  #   rename_table :train_routes, :routes
+  # end
 end
