e6a77243728f67f43cc566a06e53d6107ff2f730
Sergey Sirosh <ssv.ruby@gmail.com>
2016-11-03 16:30:20 +0300

    deploy 14

--- db/schema.rb
+++ db/schema.rb
@@ -59,7 +59,7 @@ ActiveRecord::Schema.define(version: 20161103095354) do
     t.index ["user_id"], name: "index_tickets_on_user_id", using: :btree
   end
 
-  create_table "train_routes", force: :cascade do |t|
+  create_table "routes", force: :cascade do |t|
     t.string   "title"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
