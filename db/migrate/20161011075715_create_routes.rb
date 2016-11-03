80dcf916796c7ac3b7ebc8843e8d33cb929fd0bd
Sergey Sirosh <ssv.ruby@gmail.com>
2016-11-03 16:10:56 +0300

    Deploy 12

--- db/migrate/20161011075715_create_routes.rb
+++
@@ -1,9 +0,0 @@
-class CreateTrainRoutes < ActiveRecord::Migration[5.0]
-  def change
-    create_table :routes do |t|
-      t.string :title
-
-      t.timestamps
-    end
-  end
-end
--- db/migrate/20161012085421_rename_train_route_to_route.rb
+++
@@ -1,5 +0,0 @@
-class RenameTrainRouteToRoute < ActiveRecord::Migration[5.0]
-  def change
-    rename_table :train_routes, :routes
-  end
-end
